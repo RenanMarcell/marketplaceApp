0e064e5eec13eb86dc3c3ea9b7c40403963450d7
renan <renan.vieira@luizalabs.com>
2019-05-02 01:57:52 -0300

    add project without the challenge

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const controllers = require('./app/controllers');
+const authMiddleware = require('./app/middlewares/auth');
+const validate = require('express-validation');
+const validators = require('./app/validators');
+const handle = require('express-async-handler');
+
+const routes = express.Router();
+
+routes.post('/users', validate(validators.User), handle(controllers.UserController.store));
+routes.post('/sessions', validate(validators.Session), handle(controllers.SessionController.store));
+routes.use(authMiddleware);
+
+routes.get('/ads', handle(controllers.AdController.index));
+routes.get('/ads/:id', handle(controllers.AdController.show));
+routes.post('/ads', validate(validators.Ad), handle(controllers.AdController.store));
+routes.put('/ads/:id', validate(validators.Ad), handle(controllers.AdController.update));
+routes.delete('/ads/:id', handle(controllers.AdController.delete));
+
+routes.post('/purchases', validate(validators.Purchase), handle(controllers.PurchaseController.store));
+
+module.exports = routes;
